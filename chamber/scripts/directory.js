ce828f879638e54d7f4401e195d48a5e790e8926
aaronandrei <aaronandreim@gmail.com>
2025-03-21 20:25:07 -0600

    js stuff

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -43,3 +43,23 @@ const displayCompanies = (companies) => {
 }
 
 getCompaniesData();
+
+
+// Filter between Grid and List views.
+
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('#cards');
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
