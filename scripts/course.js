1df223f3b3621237380d6c14008830919899e54c
aaronandrei <aaronandreim@gmail.com>
2025-04-05 11:47:02 -0600

    stuff

--- scripts/course.js
+++ scripts/course.js
@@ -114,3 +114,27 @@ function createCourseCard(filteredCourses) {
         document.querySelector("#courses").appendChild(card);
     });
 }
+
+const courseDetails = document.querySelector('#course-details');
+
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+        <button id="closeModal">X</button>
+        <h2>${course.subject} ${course.number}</h2>
+        <h3>${course.title}</h3>
+        <p><strong>Credits</strong>: ${course.credits}</p>
+        <p><strong>Certificate</strong>: ${course.certificate}</p>
+        <p>${course.description}</p>
+        <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+    `;
+    courseDetails.showModal();
+
+    closeModal.addEventListener('click', () => {
+        courseDetails.close();
+    });
+}
+
+courseDiv.addEventListener('click', () => {
+    displayCourseDetails(course);
+});
