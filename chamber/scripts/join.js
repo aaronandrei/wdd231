ad8e7d28c8fcc9ab780bccee524ea87f546a1f9f
aaronandrei <aaronandreim@gmail.com>
2025-04-06 11:47:24 -0600

    join updates

---
+++ chamber/scripts/join.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('yourFormId');
+const timestampField = document.getElementById('timestamp');
+
+form.addEventListener('submit', () => {
+  const currentTime = new Date().getTime() / 1000; // Get current timestamp in seconds
+  timestampField.value = currentTime;
+});
