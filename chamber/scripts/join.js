a732a18a5faef3714abec2370db78ebac006b3a7
aaronandrei <aaronandreim@gmail.com>
2025-04-06 18:48:14 -0600

    minor join updates

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,6 +2,6 @@ const form = document.getElementById('yourFormId');
 const timestampField = document.getElementById('timestamp');
 
 form.addEventListener('submit', () => {
-  const currentTime = new Date().getTime() / 1000; // Get current timestamp in seconds
-  timestampField.value = currentTime;
+    const currentTime = new Date().getTime() / 1000; // Get current timestamp in seconds
+    timestampField.value = currentTime;
 });
---
+++ chamber/scripts/thanks.js
@@ -0,0 +1,14 @@
+const info = new URLSearchParams(window.location.search);
+// console.log(info);
+
+document.querySelector('#results').innerHTML = `
+<p>Thank you, ${info.get('first')} ${info.get('last')}.</p>
+<p></p>
+<p>We appreciate you applying ${info.get('organization')} to join as a member of the Houston Chamber of Commerece.</p>
+<p>We will reach out if we have any questons at the contact information provided.</p>
+<p>Phone: ${info.get('phone')}</p>
+<p>Email: ${info.get('email')}</p>
+<p>${info.get('timestamp')}
+`
+
+// &timestamp=
